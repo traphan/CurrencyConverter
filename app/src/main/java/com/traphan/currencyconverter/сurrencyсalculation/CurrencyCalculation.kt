3d8a49a1202b1b632da3eca934cc3a4d8cb775e7
r.shakurov <renat.shakurov@compulink.com>
2019-09-02 10:29:32 +0300

    fix after merge

---
+++ app/src/main/java/com/traphan/currencyconverter/сurrencyсalculation/CurrencyCalculation.kt
@@ -0,0 +1,26 @@
+package com.traphan.currencyconverter.сurrencyсalculation
+
+import com.traphan.currencyconverter.database.entity.CurrencyJoinImage
+import com.traphan.currencyconverter.ui.CurrencyViewEntity
+import io.reactivex.Observable
+
+private fun getCalculateCurrency(currencyEntity: CurrencyJoinImage): CurrencyViewEntity {
+    var image = if (currencyEntity.imageEntity != null && currencyEntity.imageEntity.isNotEmpty()) {
+        currencyEntity.imageEntity[0].images
+    } else {
+        null
+    }
+    return CurrencyViewEntity(currencyEntity.currencyEntity.name, currencyEntity.currencyEntity.charCode, currencyEntity.currencyEntity.value / currencyEntity.currencyEntity.nominal,
+        currencyEntity.currencyEntity.nominal.toFloat(), currencyEntity.currencyEntity.value, currencyEntity.currencyEntity.nominal.toString().length,
+        image)
+}
+
+fun getCalculateAllCurrency(currencyEntities: List<CurrencyJoinImage>): Set<CurrencyViewEntity> {
+    var currencyViewEntities: Set<CurrencyViewEntity> = setOf()
+    currencyEntities.forEach{currencyViewEntities = currencyViewEntities.plus(getCalculateCurrency(it))}
+    return currencyViewEntities
+}
+fun getCalculationCurrency(currencyEntity: CurrencyViewEntity, nominal: Float): Observable<CurrencyViewEntity> {
+    return Observable.just(CurrencyViewEntity(currencyEntity.name, currencyEntity.charCode, currencyEntity.rate, nominal, currencyEntity.rate * nominal, currencyEntity.cursorPosition,
+        currencyEntity.patchImage))
+}
